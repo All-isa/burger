bcb0acf7041707370abec617c19a0f313ec3f654
allisa liberto <allisa84@yahoo.com>
2018-05-03 01:21:37 -0500

    connection issues

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -9,41 +9,29 @@ router.get("/", function(req, res) {
         var hbsObject = {
             burgers: data
         };
-    console.log(hbsObject);
+    // console.log(hbsObject);
     res.render('index', hbsObject);
     });
 });
 
-router.post("/api/burger", function(req, res) {
-    burgers.create([
-        "name", "devoured"
+router.post("/burgers", function(req, res) {
+    burgers.insertOne([
+        "burger_name"
     ], [
-        req.body.name, req.body.devoured
-    ], function(result) {
+        req.body.burger_name,
+    ], function(data) {
         res.json({ id: result.insertId });
     });
 });
 
-router.put("/api/burgers/:id", function(req, res) {
+router.put("/burgers/:id", function(req, res) {
     var condition ="id = " + req.params.id;
-    console.log("condition", condition);
+    // console.log("condition", condition);
 
-    burgers.update({
-        devoured: req.body.devoured
-    }, condition, function(result) {
+    burgers.updateOne({
+        devoured: true
+    }, condition, function(data) {
         if (result.changedRows == 0) {
-            return res.status(404).end();
-        } else {
-            res.status(200).end();
-        }
-    });
-});
-
-router.delete("api/burgers/:id", function (req, res) {
-    var condition = "id = " + req.params.id;
-
-    burgers.delete(condition, function(result) {
-        if (result.affectedRows == 0) {
             return res.status(404).end();
         } else {
             res.status(200).end();
--- models/burger.js
+++ models/burger.js
@@ -7,13 +7,13 @@ var burger = {
       });
     },
 
-insertOne: function(cols, vals, cb) {
+    insertOne: function(cols, vals, cb) {
         orm.insertOne('burgers', cols, vals, function(res) {
           cb(res);
         });
     },
 
-updateOne: function(objColVals, condition, cb) {
+    updateOne: function(objColVals, condition, cb) {
         orm.updateOne('burgers', objColVals, condition, function(res) {
           cb(res);
         });
